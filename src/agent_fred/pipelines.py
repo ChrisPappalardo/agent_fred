dfde3224fe4b94701147247a59eac5e997991a11
Chris Pappalardo <cpappala@gmail.com>
2024-06-01 15:44:14 -0700

    refactored pipelines and prompts and added embedding and llm kwargs

---
+++ src/agent_fred/pipelines.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+from haystack import Document, Pipeline
+from haystack.components.builders.prompt_builder import PromptBuilder
+from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack_integrations.components.embedders.ollama import OllamaDocumentEmbedder
+from haystack_integrations.components.embedders.ollama import OllamaTextEmbedder
+from haystack_integrations.components.generators.ollama import OllamaGenerator
+
+
+def rag_pipeline(
+        documents: list[Document],
+        prompt_template: str,
+        embedding_kwargs: dict[str, Any] = {},
+        llm_kwargs: dict[str, Any] = {},
+    ) -> Pipeline:
+    """create and return rag pipeline"""
+    # store documents with embeddings
+    document_store = InMemoryDocumentStore()
+    document_embedder = OllamaDocumentEmbedder(**embedding_kwargs)
+    docs_with_embeddings = document_embedder.run(documents)["documents"]
+    document_store.write_documents(docs_with_embeddings)
+
+    # initialize other haystack components
+    text_embedder = OllamaTextEmbedder(**embedding_kwargs)
+    retriever = InMemoryEmbeddingRetriever(document_store=document_store)
+    prompt_builder = PromptBuilder(template=prompt_template)
+    generator = OllamaGenerator(**llm_kwargs)
+
+    # create pipeline
+    pipeline = Pipeline()
+    pipeline.add_component("text_embedder", text_embedder)
+    pipeline.add_component("retriever", retriever)
+    pipeline.add_component("prompt_builder", prompt_builder)
+    pipeline.add_component("llm", generator)
+    pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+    pipeline.connect("retriever", "prompt_builder.documents")
+    pipeline.connect("prompt_builder", "llm")
+
+    return pipeline
--- src/agent_fred/run/rag.py
+++ src/agent_fred/run/rag.py
@@ -1,59 +1,39 @@
 import logging
 import os
-
-from haystack import Pipeline
-from haystack.components.builders.prompt_builder import PromptBuilder
-from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
-from haystack.document_stores.in_memory import InMemoryDocumentStore
-from haystack_integrations.components.embedders.ollama import OllamaDocumentEmbedder
-from haystack_integrations.components.embedders.ollama import OllamaTextEmbedder
-from haystack_integrations.components.generators.ollama import OllamaGenerator
+from pprint import PrettyPrinter
 
 from agent_fred.core import load_prompt, xlsx_to_haystack_docs
+from agent_fred.pipelines import rag_pipeline
 
 
-if os.environ.get("RAG_DEBUG", None) is not None:
+if debug := os.environ.get("RAG_DEBUG", None) is not None:
     logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
     logging.getLogger("haystack").setLevel(logging.INFO)
 
-prompt_filename = os.environ.get("RAG_PROMPT", "src/agent_fred/rag_prompt.txt")
+prompt_filename = os.environ.get("RAG_PROMPT", "src/agent_fred/prompts/rag.txt")
 
 
 if __name__ == "__main__":
-    print("[rag pipeline]")
-
     # get xlsx filename from user
-    filename = input("load path/filename.xlsx: ")
+    default_filename = "tests/test_data/GDPC1_1.xls"
+    filename = input(f"load [{default_filename}]: ") or default_filename
 
     # parse tables from xlsx file and convert to haystack Documents
     documents = xlsx_to_haystack_docs(filename)
 
-    # create prompt
-    prompt_template = load_prompt(prompt_filename)
-
-    # store documents with embeddings
-    document_store = InMemoryDocumentStore()
-    document_embedder = OllamaDocumentEmbedder(model="phi3")  # for docs
-    docs_with_embeddings = document_embedder.run(documents)["documents"]
-    document_store.write_documents(docs_with_embeddings)
-
-    # initialize other haystack components
-    text_embedder = OllamaTextEmbedder(model="phi3")  # for query
-    retriever = InMemoryEmbeddingRetriever(document_store=document_store)
-    prompt_builder = PromptBuilder(template=prompt_template)
-    generator = OllamaGenerator(
-        model="phi3", generation_kwargs={"temperature": 0.0},
-    )
+    if debug:
+        PrettyPrinter().pprint(documents)
 
     # create pipeline
-    pipeline = Pipeline()
-    pipeline.add_component("text_embedder", text_embedder)
-    pipeline.add_component("retriever", retriever)
-    pipeline.add_component("prompt_builder", prompt_builder)
-    pipeline.add_component("llm", generator)
-    pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-    pipeline.connect("retriever", "prompt_builder.documents")
-    pipeline.connect("prompt_builder", "llm")
+    pipeline = rag_pipeline(
+        documents=documents,
+        prompt_template=load_prompt(prompt_filename),
+        embedding_kwargs={"model": "phi3"},
+        llm_kwargs={
+            "model": "phi3",
+            "generation_kwargs": {"temperature": 0.0},
+        },
+    )
 
     # loop for q&a
     while True:
